07a62a62f6f24374e8099cfc97adfe1cd06f8416
cjihrig <cjihrig@gmail.com>
2016-02-19 11:01:04 -0500

    improve active request handling

--- fixture.js
+++ fixture.js
@@ -1,8 +1,27 @@
 // This is not part of the project, just a simple program that keeps the
 // event loop open. Run as `node -r ./lib/index.js fixture.js`
 'use strict';
+const fs = require('fs');
 const http = require('http');
 console.log('\n\nAPP STARTED');
 setInterval(function () {
+  fs.readFile(__filename, () => {
+
+  });
+
+  fs.readFile(__filename, () => {
+
+  });
+
+  fs.readFile(__filename, () => {
+
+  });
+
+  fs.readFile(__filename, () => {
+
+  });
+}, 1);
+
+setInterval(function () {
   http.get('http://www.google.com');
-}, 5000);
+}, 1000);
